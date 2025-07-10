e721c9a71bb83f039b531faee6e0e72e76baa2b4
ChoHyeongmin1225 <22100713@handong.ac.kr>
2025-07-10 16:25:46 +0900

    logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon: 변경된 심볼 */}
+        <link rel="icon" href="/images/심볼.svg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
